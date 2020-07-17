4f76cd772846f7f12790722af0e4dc9410c6db78
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-17 11:19:28 -0600

    Add ride_names to amusement park model

--- app/models/amusement_park.rb
+++ app/models/amusement_park.rb
@@ -1,4 +1,13 @@
 class AmusementPark <ApplicationRecord
   validates_presence_of :name, :admission_price
   has_many :rides
+
+  def average_thrill
+    rides.average(:thrill_rating)
+  end
+
+  def ride_names
+    x = rides.map {|ride| ride.name}
+    x.join(', ')
+  end
 end
