1089cbc42b5693537e742990c3aaaf725a57ba92
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-17 11:59:19 -0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 
   get '/mechanics', to: 'mechanics#index'
   get '/parks/:id', to: 'amusement_parks#show'
+  get '/mechanics/:id', to: 'mechanics#show'
+  post '/mechanics/:id/mechanic_rides', to: 'mechanic_rides#create'
 end
