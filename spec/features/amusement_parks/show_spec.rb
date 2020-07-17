15041ac4d4c9e9682d5540a976d928339d689f15
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-17 11:16:55 -0600

    update amusement park show page spec

--- spec/features/amusement_parks/show_spec.rb
+++ spec/features/amusement_parks/show_spec.rb
@@ -2,8 +2,8 @@ RSpec.describe 'As a visitor', type: :feature do
   before(:each) do
     @park_1 = AmusementPark.create(name: "Fly High", admission_price: 50.00)
 
-    @ride_1 = Ride.create(name: "White Dragon", thrill_rating: 8)
-    @ride_2 = Ride.create(name: "The Rocket", thrill_rating: 6)
+    @ride_1 = @park_1.rides.create(name: "White Dragon", thrill_rating: 8)
+    @ride_2 = @park_1.rides.create(name: "The Rocket", thrill_rating: 6)
   end
 
   describe 'When a user visits a amusement park\'s show page' do
@@ -17,13 +17,14 @@ RSpec.describe 'As a visitor', type: :feature do
     it "allows a user to see the names of all the rides, in alphabetical order" do
       visit "/parks/#{@park_1.id}"
 
+      save_and_open_page
       expect(page).to have_content(@ride_1.name)
       expect(page).to have_content(@ride_2.name)
     end
 
     it "allows a user to see the average thrill rating of park\'s rides" do
       visit "/parks/#{@park_1.id}"
-      
+
       expect(page).to have_content("Average Thrill Rating: 7")
     end
   end
