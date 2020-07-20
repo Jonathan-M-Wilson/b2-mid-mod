ee9e0ab1db8b2c41d7949d7e37917393f58a72a0
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-20 15:59:59 -0600

    Pass mechanic show spec test that adds a ride to the mechanics workflow

--- app/controllers/mechanic_rides_controller.rb
+++ app/controllers/mechanic_rides_controller.rb
@@ -1,9 +1,13 @@
 class MechanicRidesController < ApplicationController
 
   def create
-    mechanic = Mechanic.find(params[:id])
-    ride = Ride.find(params[:ride_id])
-    MechanicRide.create(ride_id: params[:ride_id], mechanic_id: params[:mechanic_id])
-    redirect_to "/mechanics/#{mechanic.id}"
+    MechanicRide.create(ride_mechanic_params)
+    redirect_to "/mechanics/#{params[:mechanic_id]}"
+  end
+
+  private
+
+  def ride_mechanic_params
+    params.permit(:ride_id, :mechanic_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get '/mechanics', to: 'mechanics#index'
   get '/parks/:id', to: 'amusement_parks#show'
   get '/mechanics/:id', to: 'mechanics#show'
-  post '/mechanics/:id/mechanic_rides', to: 'mechanic_rides#create'
+  post '/mechanic_rides/:mechanic_id', to: 'mechanic_rides#create'
 end
